ee2b6115784535fd0ad1dabb216ab9dfbcd24f8d
Oluwafisayomi Adebowale <61998031+the-yomi@users.noreply.github.com>
2020-03-26 00:05:47 +0100

    added new file

---
+++ Calculator.py
@@ -0,0 +1,11 @@
+print('Welcome to AreaCal')
+radius = input('What is the radius of your circle? ')
+req_val = int(radius)**2
+area = 3.142 * req_val
+msg_area = f'Your circle has an area of {area} square meters'
+print(msg_area)
+print('''Thank you for using AreaCal. 
+
+Please leave a comment.''')
+input('Comments?')
+print('Muchas Gracias')
